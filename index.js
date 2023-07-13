aa90bf08aa7d0e862c53b85f477cdaa0480e5cb9
Guilherme Santos <guilherme.santos@customerx.cx>
2023-07-13 15:01:22 -0300

    WIP

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
-import { readFile } from 'fs/promises';
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
 
-let data = JSON.parse(await readFile("filename.json", "utf8"));
+const filesDirectory = './files'
+
+const autenticacao = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'CHAVE_DO_CLIENTE'
+}
+
+const url = 'https://sandbox.api.customerx.com.br/api/v1/timelines'
+
+const directoryPath = path.join(__dirname, filesDirectory);
+fs.readdir(directoryPath, function (err, paths) {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+
+  paths.forEach(path => {
+    try {
+      console.log('Abrindo arquivo: ' + path);
+      const items = require(filesDirectory + '/' + path);
+
+      console.log('Quantidade: ' + items.length);
+
+      const filtereItems = items.filter(nota =>
+        nota.type.caption.includes('Tarefa') ||
+        nota.type.caption.includes('Reunião')
+      )
+        .map(nota => ({
+          external_id_client: nota.customer.id_legacy,
+          title: nota.description,
+          body: nota.notes,
+          created_at: nota.end_date,
+          operation: "client_notes",
+          user_id: 686
+        }))
+
+
+      console.log('Quantidade filtrada: ' + filtereItems.length);
+
+      const size = filtereItems.length;
+
+      [filtereItems[0]].forEach((item, index) => {
+        console.log('Sucesso: ' + index + '/' + size);
+        axios.post(url, item, { headers: autenticacao })
+          .then(res => {
+            console.log('Sucesso: ' + index + '/' + size);
+          })
+          .catch(err => {
+            console.error('Error no item posição: ' + index, err.message);
+          });
+      })
+
+      console.log('Fechando arquivo: ' + path);
+    } catch (error) {
+      console.error('Erro no arquivo: ' + path, error)
+    }
+  })
+});
+
+
+
+
