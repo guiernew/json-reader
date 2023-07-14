5682cccde180468cdc572e28bb04a83abe28b027
Guilherme Santos <guilherme.santos@customerx.cx>
2023-07-14 13:49:40 -0300

    wip

---
+++ altera.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const nReadlines = require('n-readlines');
+
+const filesDirectory = './files'
+
+const directoryPath = path.join(__dirname, filesDirectory);
+fs.readdir(directoryPath, function (err, paths) {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+
+  paths.forEach(path => {
+    const fullpath = filesDirectory + '/' + path;
+
+    const broadbandLines = new nReadlines(fullpath);
+
+    let line;
+    let fullText = '';
+
+    while (line = broadbandLines.next()) {
+      fullText += line.toString('utf8') + ','
+    }
+
+    fullText = '[' + fullText + ']';
+
+    fullText = fullText.replace(',]', ']')
+
+    fs.writeFile(fullpath, fullText, 'utf8', function (err) {
+      if (err) {
+        console.log('Erro em: ', path);
+      }
+      console.log('Sucesso em: ', path);
+    });
+  })
+});
+
